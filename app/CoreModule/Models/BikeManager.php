07e754f815b916d8cf9f8cc3c62d63116a92a12b
Roman Ondráček <ondracek.roman@centrum.cz>
2019-11-16 22:25:29 +0100

    Use database for bike manager
    Signed-off-by: Roman Ondráček <ondracek.roman@centrum.cz>

--- app/CoreModule/Models/BikeManager.php
+++ app/CoreModule/Models/BikeManager.php
@@ -29,63 +29,14 @@ use Nette\Database\Context;
 final class BikeManager {
 
 	/**
+	 * Database name table
+	 */
+	private const TABLE = 'bikes';
+
+	/**
 	 * @var Context Database context
 	 */
 	private $database;
-
-	/**
-	 * @var mixed[] Products
-	 */
-	private $products = [
-		[
-			'id' => 0,
-			'name' => 'Ghost Kato 4.7',
-			'img' => 'https://img.bike-pro.cz/images/SCH/MY18_KATO_4-7_AL_U_LOWBUDGED_ELECTRICBLUE_NIGHTBLACK_NEONYELLOW_18KA2029.png?vid=1&tid=30&r=A',
-			'usage' => 'XC',
-			'wheelSize' => '27,5"',
-			'size' => '17"',
-			'forkTravel' => 100,
-			'shockTravel' => null,
-			'speeds' => '3x9',
-			'price' => 500,
-		],
-		[
-			'id' => 1,
-			'name' => 'Maxbike Kyoga 27.5"',
-			'img' => 'https://img.bike-pro.cz/images/MB/2132.jpg?vid=1&tid=30&r=A',
-			'usage' => 'XC/Trail',
-			'wheelSize' => '27,5"',
-			'size' => '17"',
-			'forkTravel' => 120,
-			'shockTravel' => null,
-			'speeds' => '2x10',
-			'price' => 600,
-		],
-		[
-			'id' => 2,
-			'name' => 'Ghost Kato FS 3 27,5"',
-			'img' => 'https://img.bike-pro.cz/images/SCH/17AM1010_MY17_KATO_FS_3_AL_27-5_U_NIGHTBLACK_RIOTGREEN_NEONRED.jpg?vid=1&tid=30&r=A',
-			'usage' => 'Trail',
-			'wheelSize' => '27,5"',
-			'size' => 'M',
-			'forkTravel' => 130,
-			'shockTravel' => 130,
-			'speeds' => '3x10',
-			'price' => 750,
-		],
-		[
-			'id' => 3,
-			'name' => 'Cannondale Jekyll 3',
-			'img' => 'https://singlekras.rezervator.cz/files/items/images/img_7530d4192b569ab8664a114ce4049265.jpg?1552051310',
-			'usage' => 'Trail/Enduro',
-			'wheelSize' => '27,5"',
-			'size' => 'M',
-			'forkTravel' => 150,
-			'shockTravel' => 150,
-			'speeds' => '1x12',
-			'price' => 900,
-		],
-	];
 
 	/**
 	 * Constructor
@@ -95,12 +46,25 @@ final class BikeManager {
 		$this->database = $database;
 	}
 
+	/**
+	 * Lists all bikes
+	 * @return mixed[] Bikes
+	 */
 	public function list(): array {
-		return $this->products;
+		return $this->database->table(self::TABLE)
+			->fetchAll();
 	}
 
+	/**
+	 * Returns a bike
+	 * @param int $id Bike ID
+	 * @return mixed[] Bike
+	 */
 	public function get(int $id): array {
-		return $this->products[$id];
+		return $this->database->table(self::TABLE)
+			->where('id', $id)
+			->fetch()
+			->toArray();
 	}
 
 }
